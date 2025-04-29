386281fb1c574018a2537d84c65b988753f98f51
Trolzer <39714370+Trolzer@users.noreply.github.com>
2025-04-28 20:34:52 -0500

    fix images

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 
     <div class="tile" onclick="window.location='restaurant.php?id=5'">
         <div class="tileContent">
-            <img src="images/Timbers/TimbersGrille.jpg" class="tileImage">
+            <img src="images/Timbers/1.png" class="tileImage">
             <p class="tileDesc">A Grille with classic burgers, milkshakes, fries, and cheese balls.  Every month they have a special, a new food item, alonside a new milkshake flavor!  A good place to sit and eat while watching the game.</p>
                        
         </div>
@@ -21,14 +21,14 @@
 
     <div class="tile" onclick="window.location='restaurant.php?id=3'">
         <div class="tileContent">
-            <img src="images/Starbucks/starbucks.jpg" class="tileImage">
+            <img src="images/Starbucks/1.png" class="tileImage">
             <p class="tileDesc">This well known brand can be found all around campus!  Known for their coffees and other treats they are the prefect way to start any day!</p>
         </div>
     </div>
 
     <div class="tile" onclick="window.location='restaurant.php?id=4'">
         <div class="tileContent">
-            <img src="images/Subway/subway.jpg" class="tileImage">
+            <img src="images/Subway/1.png" class="tileImage">
             <p class="tileDesc">Eat fresh every day at this well known sandwich place.  Customize your sandwich to have something diffrent every day, or just stick with your favorite!  A good place if you are looking to grab a quick lunch.</p>
                        
         </div>
@@ -36,21 +36,21 @@
 
     <div class="tile" onclick="window.location='restaurant.php?id=1'">
         <div class="tileContent">
-            <img src="images/Landing/landing.jpg" class="tileImage">
+            <img src="images/Landing/2.jpg" class="tileImage">
             <p class="tileDesc">Grab your sides and customize a personal size pizza today!  Perfect for grabbing and taking back home for lunch or dinner, or grabbing a few snacks inbetween classes.</p>
                        
         </div>
     </div>
     <div class="tile" onclick="window.location='restaurant.php?id=6'">
         <div class="tileContent">
-            <img src="images/Qdoba/qudoba.jpg" class="tileImage">
+            <img src="images/Qdoba/1.jpg" class="tileImage">
             <p class="tileDesc">Get mexican food fast, fresh, and casual at this resturant.  They have options for anytime of the day, perfect for whenever you get that craving for some mexican food.</p>
                        
         </div>
     </div>
     <div class="tile" onclick="window.location='restaurant.php?id=2'">
         <div class="tileContent">
-            <img src="images/Deli/Deli.jpg" class="tileImage">
+            <img src="images/Deli/1.png" class="tileImage">
             <p class="tileDesc">A place with a little bit of everything for anyone.  This resutrant is best known for thier sandwichs and potatoes, perfect for when are you looking to grab something for lunch or dinner!</p>
                        
         </div>
